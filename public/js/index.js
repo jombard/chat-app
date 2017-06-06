b6078549f5f0d8d00e9af785f8ad462db0453685
Jon Lombard <jombard@hotmail.com>
2017-06-06 22:08:07 +0100

    Add geolocation support

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,13 @@ socket.on('newMessage', function (message) {
 	$('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+	var li = $("<li />").text(`${message.from}: `);
+	var a = $("<a />").attr("target","_blank").text('My current location').attr('href', message.url);
+
+	$('#messages').append(li.append(a));
+});
+
 $('#message-form').on('submit', function (e) {
 	e.preventDefault();
 
@@ -24,5 +31,21 @@ $('#message-form').on('submit', function (e) {
 		text: $('[name=message]').val()
 	}, function () {
 
+	});
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click', function () {
+	if(!navigator.geolocation) {
+		return alert('Geolocation not supprted by your browser.');
+	}
+
+	navigator.geolocation.getCurrentPosition(function (position) {
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+	}, function () {
+		alert('Unable to fetch location.');
 	});
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate correct message object', () => {
@@ -9,5 +9,18 @@ describe('generateMessage', () => {
 
 		expect(message.createdAt).toBeA('number');
 		expect(message).toInclude({from, text});
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		var from = 'Jen';
+		var latitude = 1;
+		var longitude = 2;
+		var url = 'https://www.google.com/maps?q=1,2'
+		var message = generateLocationMessage(from, latitude, longitude);
+
+		expect(message.createdAt).toBeA('number');
+		expect(message).toInclude({from, url});
 	});
 });
