b2c9a3a2137f01d4d44876add838871e92497b1c
Jon Lombard <jombard@hotmail.com>
2017-06-06 09:12:05 +0100

    create generatemessage utility

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'jen',
-		text: 'Hey. Some more text.'
-	});
 });
 
 socket.on('disconnect', function () {
