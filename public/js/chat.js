c6ad79db947b58010f23903f838dd92f482307d4
Jon Lombard <jombard@hotmail.com>
2017-06-07 10:20:52 +0100

    Wired up people list in chat rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,11 +15,33 @@ function scrollToBottom() {
 }
 
 socket.on('connect', function () {
+	var params = $.deparam(window.location.search);
+
+	socket.emit('join', params, function (err) {
+		if (err) {
+			alert(err);
+			window.location.href = '/';
+		} else {
+			console.log('No error');
+		}
+	});
+});
+
+socket.on('connect', function () {
 	console.log('Connected to server');
 });
 
 socket.on('disconnect', function () {
 	console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+	var ol = $('<ol />');
+	users.forEach(function (user) {
+		ol.append($('<li />').text(user));
+	});
+
+	$('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
